c95eff8c6eb3af5f57e3f4a033045444ee3d0656
Japhy <github@pearachute.com>
2012-06-14 00:25:56 -0400

    slides!

---
+++ slides/13-stemming.py
@@ -0,0 +1,7 @@
+#http://pypi.python.org/pypi/stemming/1.0
+
+"""
+Python implementations of the Porter, Porter2, Paice-Husk, and Lovins stemming algorithms for English. These implementations are straightforward and efficient, unlike some Python versions of the same algorithms available on the Web. This package is an extraction of the stemming code included in the Whoosh search engine.
+
+Note that these are pure Python implementations. Python wrappers for, e.g. the Snoball stemmers and the C implementation of the Porter stemmer are available on PyPI and will be faster if using compiled code is an option for you.
+"""
---
+++ slides/18-tfidf.py
@@ -0,0 +1,28 @@
+tweet = 'like this is like totally the best chipy ever'
+
+tf = {
+    'like':     2,
+    'this':     1,
+    'totally':  1,
+    'best':     1,
+    'chipy':    1,
+    'ever':     1,
+    }
+
+df = {
+    'like':     100,
+    'this':     100,
+    'totally':  100,
+    'best':     50,
+    'chipy':    13,
+    'ever':     100,
+    }
+
+scores = {
+    'like':     .02,
+    'this':     .01,
+    'totally':  .01,
+    'best':     .02,
+    'chipy':    .0769230,
+    'ever':     .01,
+    }
